99415d3c9bc84f2c343801c217eb8f3c500eff34
Kittipon <wakizashixd@gmail.com>
2020-09-27 19:32:56 +0700

    Lab07 change ArrayList to DB

--- src/main/java/th/ac/ku/atm/service/CustomerService.java
+++ src/main/java/th/ac/ku/atm/service/CustomerService.java
@@ -2,7 +2,9 @@ package th.ac.ku.atm.service;
 
 
 import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import th.ac.ku.atm.data.CustomerRepository;
 import th.ac.ku.atm.model.Customer;
 
 import javax.annotation.PostConstruct;
@@ -11,37 +13,36 @@ import java.util.List;
 
 @Service
 public class CustomerService {
+    private CustomerRepository repository;
 
-    private List<Customer> customerList;
-
-    @PostConstruct
-    public void postConstruct() {
-        this.customerList = new ArrayList<>();
+    public CustomerService(CustomerRepository repository) {
+        this.repository = repository;
     }
+
 
     public void createCustomer(Customer customer) {
 //        hash pin for customer
         String hashPin = hash(customer.getPin());
         customer.setPin(hashPin);
-        customerList.add(customer);
+        repository.save(customer);
     }
 
-    public List<Customer> getCustomers() {
-        return new ArrayList<>(this.customerList);
-    }
-
-        private String hash (String pin){
+    private String hash (String pin){
             String salt = BCrypt.gensalt(12);
             return BCrypt.hashpw(pin, salt);
-        }
+    }
 
 
     public Customer findCustomer(int id) {
-        for (Customer customer : customerList) {
-            if (customer.getId() == id)
-                return customer;
-        }
-        return null;
+      try {
+          return repository.findById(id);
+      } catch (EmptyResultDataAccessException e) {
+          return null;
+      }
+    }
+
+    public List<Customer> getCustomers() {
+        return repository.findAll();
     }
     public Customer checkPin(Customer inputCustomer) {
         // 1. หา customer ที่มี id ตรงกับพารามิเตอร์
